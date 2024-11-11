ff6a8e9e04b42ad057a681efa7ad98f47b6c955e
Your Name <you@example.com>
2024-11-11 15:54:33 +0900

    lscount,how_hand,winの実装

--- janken.py
+++ janken.py
@@ -1,9 +1,32 @@
+import random
+
 def lscount(ls,self):
     if len(ls) < len(self) or ls == [] or self == []:
         return 0
     sa = len(ls) - len(self) + 1
     out = 0
+    for i in range(sa):
+        if ls[i:i+len(self)] == self:
+            out = out + 1
+    return out
 
 
-def how_hand(self):#何手ごとの統計を取ればよいか
-    pass
+def how_hand(ls,self):#何手ごとの統計を取ればよいか
+    n = len(ls)-1
+    count = 0
+    conti = True
+    while conti:
+        lis = ls[-n:]
+        lis.append(self)
+        if lscount(ls,lis) >= 2:
+            return n+1
+        elif n == 0:
+            if self in ls:
+                return 1
+            else:
+                return 0
+        n = n - 1
+
+def win(self):
+    dic = {"g":"p","c":"g","p":"c"}
+    return dic[self]
--- test_janken.py
+++ test_janken.py
@@ -2,13 +2,23 @@ from janken import *
 
 def test_lscount():
     ls = [0,1,2,3,4,5,6,3,4,5,3,4,0,1]
-    lscount([],[3]) == 0
-    lscount(ls,[]) == 0
-    lscount([],[]) == 3
-    lscount(ls,[3]) == 3
-    lscount(ls,[4]) == 3
-    lscount(ls,[3]) == 3
+    assert lscount([],[3]) == 0
+    assert lscount(ls,[]) == 0
+    assert lscount([],[]) == 0
+    assert lscount(ls,[3]) == 3
+    assert lscount(ls,[4]) == 3
+    assert lscount(ls,[3,4]) == 3
+    assert lscount(ls,[3,4,5]) == 2
+    assert lscount(ls,[0,1]) == 2
 
 def test_how_hand():
-    pass
+    ls = [0,1,2,3,4,5,6,3,4,5,3,4,0,1,2,3,0,1]
+    assert how_hand(ls,2) == 3
+    assert how_hand(ls,1) == 1
+    ls = [0,1,2,3,4,5,6,3,4,5,3,4,0,1,2,3,0,1,2]
+    assert how_hand(ls,3) == 4
 
+def test_win():
+    assert win("g") == "p"
+    assert win("c") == "g"
+    assert win("p") == "c"
