e0227110fe19899eda5d62b1845a90d98230ce96
Your Name <you@example.com>
2024-11-16 21:29:16 +0900

    実装

--- janken.py
+++ janken.py
@@ -1,6 +1,6 @@
 import random
 
-def lscount(ls,self):
+def lscount(ls,self):#ある連続する要素の出現回数
     if len(ls) < len(self) or ls == [] or self == []:
         return 0
     sa = len(ls) - len(self) + 1
@@ -30,3 +30,124 @@ def how_hand(ls,self):#何手ごとの統計を取ればよいか
 def win(self):
     dic = {"g":"p","c":"g","p":"c"}
     return dic[self]
+
+def clip(ls,self):
+    if ls == []:
+        return []
+    n = how_hand(ls,self)
+    ls.append(self)
+    if n == 0:
+        return ls
+
+    out = []
+    sa = len(ls) - n
+    for i in range(sa):
+        if ls[i:i+n] == ls[-n:]:
+#            print(i,n,ls[i:i+n],ls[-n:],ls)
+            out.append(ls[i+n])
+    return out
+
+def max_pic(ls):
+    if ls == []:
+        return []
+    dic = {}
+    for i in ls:
+        if i in list(dic.keys()):
+            dic[i] = dic[i] + 1
+        else:
+            dic[i] = 1
+    out = []
+    n = max(list(dic.values()))
+    for i in list(dic.keys()):
+        if dic[i] == n:
+            out.append(i)
+    return out
+
+def janwin(ls):
+    if ls == []:
+        return random.choice(["g","c","p"])
+    if len(ls) == 3:
+        return random.choice(["g","c","p"])
+    if len(ls) == 1:
+        return win(ls[0])
+    if win(ls[0]) == ls[1]:
+        return ls[1]
+    else:
+        return ls[0]
+
+def jprint(me,cp,log):
+
+    print("total game :",len(log))
+    if len(log) == 0:
+        pass
+    else:
+        cpwin = 0
+        cpdraw = 0
+        mewin = 0
+        for i in log:
+            if i == "cp":
+                cpwin += 1
+            elif i == "draw":
+                cpdraw += 1
+            else:
+                mewin += 1
+        print("cp total win :" ,cpwin)
+        print("total draw :" ,cpdraw)
+        print("me total  win :" ,mewin)
+        print("cp win% :" , round((cpwin*100)/len(log)))
+        print("draw% :" ,round((cpdraw*100)/len(log)))
+        print("me win% :" ,round((mewin*100)/len(log)))
+
+        if len(log) > 10:
+            cpwin = 0
+            cpdraw = 0
+            mewin = 0
+            for i in log[-10:]:
+                if i == "cp":
+                    cpwin += 1
+                elif i == "draw":
+                    cpdraw += 1
+                else:
+                    mewin += 1
+            print("cp last10 win :" ,cpwin)
+            print("last10 draw :" ,cpdraw)
+            print("me last10 win :" ,mewin)
+    print(log)
+    if log[-1] == "draw":
+        result = "draw"
+    else:
+        result = log[-1] + " win"
+    print("\n" + "CP:" + cp)
+    print(result)
+    print("you:" + me + "\n")
+
+def judge(me,cp):
+    if me == cp:
+        return "draw"
+    if win(cp) == me:
+        return "me"
+    else:
+        return "cp"
+
+"""
+g 4
+c 3
+p 3
+g w3 d4 l3
+c w3 d3 l4
+p w4 d3 l3
+"""
+
+cphand = random.choice(["g","c","p"])
+mehand = input("your hand ")
+handlog = [mehand]
+winlog = [judge(mehand,cphand)]
+jprint(mehand,cphand,winlog)
+n = 1
+while not(n == 100):
+    cphand = janwin(max_pic(clip(handlog,mehand)))
+    mehand = input("your hand ")
+    handlog.append(mehand)
+    winlog.append(judge(mehand,cphand))
+    jprint(mehand,cphand,winlog)
+    n += 1
--- test_janken.py
+++ test_janken.py
@@ -22,3 +22,39 @@ def test_win():
     assert win("g") == "p"
     assert win("c") == "g"
     assert win("p") == "c"
+
+def test_clip():
+    ls = [0,1,2,3,4,5,6,3,4,5,3,4,0,1,2,3,0,1]
+    assert clip([],0) == []
+    assert clip(ls,2) == [3,3]
+    assert clip(ls,1) == [2,2,2]
+    ls = [0,1,2,3,4,5,6,3,4,5,3,4,0,1,2,3,0,1,2]
+    assert clip(ls,3) == [4,0]
+
+def test_max_pic():
+    assert max_pic([]) == []
+    ls = [0,1,2,3]
+    assert max_pic(ls) == ls
+    ls = [0,0,0]
+    assert max_pic(ls) == [0]
+    ls = [0,0,1,1,2,3,3,3,4,4,4]
+    assert max_pic(ls) == [3,4]
+
+def test_janwin():
+    ls = ["g","c","p"]
+    for i in range(100):
+        j = janwin([])
+        assert len(j) == 1
+        assert j in ls
+    assert janwin(["g"]) == "g"
+    ls = ["g","c","p"]
+    for i in range(100):
+        j = janwin(ls)
+        assert len(j) == 1
+        assert j in ls
+    assert janwin(["g","c"]) == "g"
+    assert janwin(["p","c"]) == "c"
+    assert janwin(["g","p"]) == "p"
+
+#jprint("g","c",["me"])
+#jprint("g","c",["me","cp","draw","me","cp","draw","me","cp","draw","me","cp","me","me","me","me",])
